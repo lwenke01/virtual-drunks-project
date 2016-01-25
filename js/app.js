1376faff0ce0677c1b086a91d1166d2bb0e70242
Joseph McClenahan <jmcclena94@gmail.com>
2016-01-25 10:38:49 -0800

    created computerPick, computerChoice, and computerIntro functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
-'use strict'
+// 'use strict'
+
+var textID = "id of html text area";
+var imageID = "id of html card image area";
+var flipID = "id of html card flip image area";
+var opponentInsult = "pick of opponent insult";
+var pathToDeckOfCardImage = "image of deck of cards";
+var cardArrayLength = "length of array of cards";
+var pathToFlipCardImage = "array of card image paths";
+var arrayOfCardValues = "array of card values";
+var soberQuote = 'sober quote message';
+
+
+var computerCardValue = 0;
+var totalComputerPicks = 0;
+var computerPick = 0;
+var randomCardIndex = 0;
+function computerChoice () {
+  if (totalComputerPicks === 0) {
+    computerPick = Math.floor(Math.random() * (2)) + 1;
+  } else {
+    computerPick = Math.floor(Math.random() * (3)) + 1;
+  }
+}
+function computerIntro () {
+  textID.textContent = opponentInsult;
+  imageID.src = pathToDeckOfCardImage;
+  randomCardGenerator();
+  computerCardValue = arrayOfCardValues[randomCardIndex];
+  flipID.src = pathToFlipCardImage[randomCardIndex];
+  arrayOfCardValues.splice(randomCardIndex,1);
+  pathToFlipCardImage.splice(randomCardIndex,1);
+  computerPick();
+}
+function computerPick () {
+  textID.textContent = soberQuote;
+  computerChoice();
+  if (computerPick === 1 || computerPick === 2) {
+    computerNewCard();
+  } else {
+    userIntro();
+  }
+}
+function computerNewCard () {
+  randomCardGenerator();
+}
